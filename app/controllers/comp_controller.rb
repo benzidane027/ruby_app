9d91ae91eb433efad632b000d99438783136bfb9
benzidane027 <benzidan27@yahoo.com>
2023-04-27 15:35:26 +0100

    pc_work:

--- app/controllers/comp_controller.rb
+++ app/controllers/comp_controller.rb
@@ -1,12 +1,23 @@
+# frozen_string_literal: true
+
 class CompController < ApplicationController
   def add
-    render json: {}, status: 200
+    user_id = ApplicationController.decode_token(request.headers['Authorization'].to_s.split[1])[:data][0]['user_id']
+    complaint = Complaint.new(
+      user_id: user_id, description: params['description'].to_s,
+      comp_type: params['comp_type'],
+      address: params['address']
+    )
+    if complaint.validate
+      complaint.save!
+      return render json: {}, status: 200
+    end
+    render json: {}, status: 400
   end
 
   def get
     user_id = ApplicationController.decode_token(request.headers['Authorization'].to_s.split[1])[:data][0]['user_id']
-    @user = User.find_by_id(1)
-
+    @user = User.find_by_id(user_id)
     render json: { data: @user.complaints }, status: 200
   end
 end
