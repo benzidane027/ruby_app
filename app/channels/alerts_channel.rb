a4e3cdce3490fa303a716d62a2984a2b881befe0
benzidane027 <benzidan27@yahoo.com>
2023-07-03 01:08:09 +0100

    fix channels

--- app/channels/alerts_channel.rb
+++ app/channels/alerts_channel.rb
@@ -1,5 +1,14 @@
-class AlertsChannel < ActionCable::Channel
+class AlertsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "messages"
+    puts "**** welcome ****"
+  end
+
+  def unsubscribed
+    puts "**** bye bye ****"
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast("messages", { "hello" => "hi hi" })
   end
 end
