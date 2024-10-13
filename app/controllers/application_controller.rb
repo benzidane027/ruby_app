e2b2474cacbfb8e4c75c0198c76b924534af5ee1
benzidane027 <benzidan27@yahoo.com>
2024-10-13 12:54:10 +0100

    adapting rails api to use active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base #ActionController::API
   # for multiple response
   include ActionController::MimeResponds
 
   REFRESH_EXP = 3600 * 24 * 5 # six hours
   ACCESS_EXP = 3600 * 24 # one hours
-  SECRET = '8125aqN?!sollz_!=yt'.freeze
+  SECRET = "8125aqN?!sollz_!=yt".freeze
 
   def self.encode_token(user_id:, type: :access)
     payload = {}
@@ -13,13 +13,13 @@ class ApplicationController < ActionController::API
     payload[:type] = type
     token = JWT.encode(payload, SECRET)
     ActiveRefresToken.new(token: token, user_id: user_id).save if type == :refresh
-    { data: token, code: :good, msg: '' }
+    { data: token, code: :good, msg: "" }
   end
 
   def self.decode_token(token)
     data = JWT.decode(token, SECRET)
-    { data: data, code: :good, msg: '' }
+    { data: data, code: :good, msg: "" }
   rescue StandardError
-    { data: '', code: :bad, msg: 'token is invalid' }
+    { data: "", code: :bad, msg: "token is invalid" }
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ require_relative "../app/midleware/auth_midleware"
 Bundler.require(*Rails.groups)
 Dotenv::Railtie.load
 
+# for active admin (cuz we use rails api)
+config.middleware.use ActionDispatch::Flash
+config.middleware.use Rack::MethodOverride
+config.middleware.use ActionDispatch::Cookies
+
 module TestApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
